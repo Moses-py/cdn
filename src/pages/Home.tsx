d61335715051e8435052d1a9f4c022ee0f46d522
Moses Chukwunekwu <dantereus1@gmail.com>
2023-01-09 06:28:25 +0100

    Minor fixes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Footer } from "../components/footer/Footer";
 import { Business } from "../features/businesses/Business";
 import { Contact } from "../features/contact/Contact";
 import { Hero } from "../features/hero/Hero";
@@ -6,7 +7,6 @@ import { Navbar } from "../features/hero/Navbar";
 import { Newsletter } from "../features/newsletter/Newsletter";
 import { Testimonial } from "../features/testimonials/Testimonial";
 import { Core } from "../features/values/Core";
-import { Footer } from "../footer/Footer";
 import { DesignCircle, DesignCircleDuo } from "../styles/Home/hero/Hero.styles";
 
 export const Home: React.FunctionComponent = () => {
